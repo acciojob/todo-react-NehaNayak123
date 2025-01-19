290745d1004766bb38a91f35af7bfc289484bca3
Neha Nayak <nayakneha951@gmail.com>
2025-01-19 15:01:18 +0530

    first

---
+++ src/components/ToDoList.js
@@ -0,0 +1,49 @@
+import { useState } from "react";
+import "./toDoList.css"
+const ToDoList =()=>{
+    const [todos, setTodos]=useState([]);
+    const [newTodo, setNewToDo]=useState('');
+
+    const handleInput= (e)=>{
+        setNewToDo(e.target.value)
+    }
+    const addToDo= ()=>{
+        if(newTodo.trim()){   
+            setTodos([...todos, newTodo]);
+            setNewToDo('');
+        } 
+        console.log("todos", todos);
+        
+    }
+    const removeToDo=(index)=>{
+        setTodos(todos.filter((todo, i)=> i !== index));
+    }
+    return (
+        <div className="container">
+            <h1>To-Do List</h1>
+            <div className="main">
+                <div>
+                    <input 
+                        placeholder="Write Task Here"
+                        type="text"
+                        value={newTodo}
+                        onChange={handleInput}
+                    />
+                    <button onClick={addToDo}>Add Todo</button>
+                </div>
+                <ul className="lists">
+                    {
+                        todos.map((todo, index)=>
+                            <li key={index}>
+                                {todo}
+                                <button onClick={()=>removeToDo(index)}>Delete</button>
+                            </li>
+                        )
+                    }
+                </ul>
+            </div>
+        </div>
+    )
+}
+
+export default ToDoList;
